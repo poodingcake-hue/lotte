045bf5e941075c29ed2eb51aec9395ef0a05e508
poodingcake-hue <poodingcake@gmail.com>
2025-12-25 14:38:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', (e) => {
+  self.skipWaiting(); // 설치 즉시 활성화
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => caches.delete(key))); // 기존 캐시 싹 제거
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // 캐시 무시하고 네트워크에서 먼저 가져오기
+  e.respondWith(fetch(e.request));
+});
